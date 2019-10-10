1892a86fb93e9e030c4be8ab5f64b2b73900fcb5
JamesASchulze <james@jamesschulze>
2019-10-09 22:10:52 -0500

    Page 2 is done.

--- javascript/assignments/06/02.js
+++ javascript/assignments/06/02.js
@@ -1,51 +1,42 @@
 window.onload = function() {
-   
-   // Get the element with the a tag.
-   var links = document.getElementsByTagName('a');
-   
-   // Did I get the element?
-   console.log(links);
-   
-   // This page only has one 'a' tag but it is in a list. 
-   for (var i=0; i<links.length; i++) {      
-      // Get data about the image.
-      if (links[i].getAttribute("class") == "pupImage");
-      var imageLink = links[i].getAttribute("href");
-      var imageAlt = links[i].getAttribute('title');
-   }
 
-   // Element to insert image into.
-   var elPosition = document.getElementById('dogPhoto');
-   console.log(elPosition);
+   // Get the element with the a tag.
+   var link = document.getElementById('pupImage');
+
+   // Did I get the element?
+   console.log(link);
+
+   // Add the event listener and call the pupIn function and stop it from doing what it would normally do..
+   link.addEventListener('click', pupIn, false);
+
+}
+
+function pupIn(e) {
+
+   // log the event
+   console.log(e);
+
+   // Get the href and the title of the calling element to use as the 'src' and 'alt' attributes.
+   var imageLink = e.target.getAttribute("href");
+   var imageAlt = e.target.getAttribute('title');
+
+   // log the link and alt caption.
+   console.log(imageLink);
+   console.log(imageAlt);
+   e.preventDefault();
 
    // Create and fill a new img element.
    var elImg = document.createElement('img');
    elImg.setAttribute('src', imageLink);
    elImg.setAttribute('alt', imageAlt);
-   
+
+   // Element to insert image into.
+   var elPosition = document.getElementById('dogPhoto');
+
+   // Log elPosition.
+   console.log(elPosition);
+
    // Add the img element to the DOM.
    elPosition.appendChild(elImg);
-   return false;
 
 }
-
-// onclick="showDogPhoto(this); return false;" 
-
-// <script>
-// window.onload = function() {
-
-
-// var links = document.getElementsByTagName("a");
-// console.log(links);
-// for (var i=0; i<links.length; i++) {
-// 	console.log(i);
-// 	console.log(links[i]);
-// 	console.log(links[i].getAttribute("class"));
-// 	console.log(links[i].getAttribute("class") == "popup");
-// 	if (links[i].getAttribute("class") == "popup") {
-// 		links[i].onclick = function() {
-// 			console.log(this.getAttribute("href"));
-// 			popUp(this.getAttribute("href"));
-// 			return false
-// 		}
-// 	}
