aa919d8aee801fb3bb876edc15b8443f799fbdee
JamesASchulze <james@jamesschulze>
2019-10-16 22:45:14 -0500

    Toggle Finished

--- javascript/assignments/07/toggle.js
+++ javascript/assignments/07/toggle.js
@@ -1,5 +1,18 @@
 $(function() {
 
-    var $toggle = $('h2:has(#showHide)')
+   var $h2 = $('h2');
+   $h2.on('click', function(e) {
+
+      var $toggle = $('#showHide');
+
+      $toggle.toggle("slow");
+
+      if ($h2.text() == "Hide") {
+         $h2.text('Show');
+      } else {
+         $h2.text('Hide');
+      }
+
+   });
 
 });
